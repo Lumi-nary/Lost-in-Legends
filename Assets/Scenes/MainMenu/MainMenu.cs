bf106724e153b421ece7cbb99ace3f56e1addc86
UwUMapleNeko <72373076+Lumi-nary@users.noreply.github.com>
2024-11-12 21:17:34 +0800

    Prototype Completion

--- Assets/Scenes/MainMenu/MainMenu.cs
+++ Assets/Scenes/MainMenu/MainMenu.cs
@@ -2,30 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public GameObject volume;
     public GameObject mainMenu;
-    
-   
+    public GameObject aboutUs;
+    public Button playButton;
+    public Button optionButton;
+    public Button aboutButton;
+    public Button quitButton;
+    public Button backButton;
+
+    public void Start()
+    {
+        AudioManager.Instance.PlayAmbient(AmbientKey.ForestAmbience);
+
+        Button btn = playButton.GetComponent<Button>();
+        Button btn2 = optionButton.GetComponent<Button>();
+        Button btn3 = aboutButton.GetComponent<Button>();
+        Button btn4 = quitButton.GetComponent<Button>();
+        Button btn5 = backButton.GetComponent<Button>();
+
+        btn.onClick.AddListener(GameStart);
+        btn2.onClick.AddListener(Volume);
+        btn3.onClick.AddListener(AboutUs);
+        btn4.onClick.AddListener(Quit);
+        btn5.onClick.AddListener(Back);
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
     public void Quit()
     {
         Application.Quit();
     }
-
-    public void Start()
-    {
-        SceneManager.LoadScene("StartScene");
-    }
-
     public void Back()
     {
         volume.SetActive(false);
+        aboutUs.SetActive(false);
         mainMenu.SetActive(true);
     }
     public void Volume()
     {
         volume.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+    public void AboutUs()
+    {
+        aboutUs.SetActive(true);
         mainMenu.SetActive(false);
     }
 }
--- Assets/Scenes/MainMenu/VolumeSettings.cs
+++ Assets/Scenes/MainMenu/VolumeSettings.cs
@@ -10,21 +10,67 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
+    [SerializeField] private Slider ambientSlider;
+
+    // Constants for volume settings
+    private const float MIN_VOLUME = 0.001f; // -80dB
+    private const float MAX_VOLUME = 1f; // 0dB
+    private const string MUSIC_VOLUME_KEY = "MusicVolume";
+    private const string SFX_VOLUME_KEY = "SFXVolume";
+    private const string AMBIENT_VOLUME_KEY = "AmbientVolume";
+
+    private void Awake()
+    {
+        // Load saved volumes
+        LoadVolumes();
+    }
 
     private void Start()
     {
-        SetMusicVolume();
+        // Add listeners to sliders
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        ambientSlider.onValueChanged.AddListener(SetAmbientVolume);
+
+        // Initialize volumes with current slider values
+        SetMusicVolume(musicSlider.value);
+        SetSFXVolume(sfxSlider.value);
+        SetAmbientVolume(ambientSlider.value);
     }
-        public void SetMusicVolume()
+    private void LoadVolumes()
     {
-        float volume = musicSlider.value;
-        myMixer.SetFloat("music", Mathf.Log10 (volume) *20);
+        // Load saved volumes or set to default (0.75f = 75%)
+        musicSlider.value = PlayerPrefs.GetFloat(MUSIC_VOLUME_KEY, 0.75f);
+        sfxSlider.value = PlayerPrefs.GetFloat(SFX_VOLUME_KEY, 0.75f);
+        ambientSlider.value = PlayerPrefs.GetFloat(AMBIENT_VOLUME_KEY, 0.75f);
     }
 
-    public void SetsfxVolume()
+    public void SetMusicVolume(float volume)
     {
-        float SFX = sfxSlider.value;
-        myMixer.SetFloat("sfx", Mathf.Log10(SFX) * 20);
+        // Clamp the volume to prevent -infinity when converting to log10
+        volume = Mathf.Clamp(volume, MIN_VOLUME, MAX_VOLUME);
+        myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(MUSIC_VOLUME_KEY, volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        volume = Mathf.Clamp(volume, MIN_VOLUME, MAX_VOLUME);
+        myMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(SFX_VOLUME_KEY, volume);
+    }
+
+    public void SetAmbientVolume(float volume)
+    {
+        volume = Mathf.Clamp(volume, MIN_VOLUME, MAX_VOLUME);
+        myMixer.SetFloat("ambient", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat(AMBIENT_VOLUME_KEY, volume);
+    }
+
+    private void OnDisable()
+    {
+        // Save volumes when script is disabled
+        PlayerPrefs.Save();
     }
 
 }
